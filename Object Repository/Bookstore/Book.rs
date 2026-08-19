4923e11ae04b9397658b7df1c0f4e0c0f2b44bff
DandyWork <dandyekowork@gmail.com>
2026-08-19 20:37:44 +0700

    add automation Bookstore

---
+++ Object Repository/Bookstore/Book.rs
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Book</name>
+   <tag></tag>
+   <elementGuidId>35be60ae-eadb-4ae5-8c8d-2d55ff68fadc</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//li[.//h2[text()='${bookTitle}']]</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
